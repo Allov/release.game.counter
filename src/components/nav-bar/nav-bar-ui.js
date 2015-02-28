3927d6ff3fbcf5827fdff6be38a56032f8234c4f
Allov <frederik.charest@gmail.com>
2015-02-28 13:05:51 -0500

    Now able to switch between language. #11

--- src/components/nav-bar/nav-bar-ui.js
+++ src/components/nav-bar/nav-bar-ui.js
@@ -1,10 +1,26 @@
-define(['text!./nav-bar.html','nav-bar', 'router', 'jquery'],
-    function(template, navBar, router, $) {
+define(['text!./nav-bar.html','nav-bar', 'router', 'knockout', 'jquery', 'knockout-i18next'],
+    function(template, navBar, router, ko, $, knockoutI18next) {
         'use strict';
 
         function NavBarViewModel() {
             var self = this;
             self.menus = navBar.menus;
+            self.currentLanguage = ko.pureComputed(function() {
+                return knockoutI18next.lng() === 'fr' ? 'Français' : 'English';
+            });
+            
+            self.changeLanguageFR = function() {
+                knockoutI18next.lng('fr');
+            };
+            
+            self.changeLanguageEN = function() {
+                knockoutI18next.lng('en');
+            };
+            
+            self.toggleLanguage = function() {
+                knockoutI18next.lng(knockoutI18next.lng() === 'fr' ? 'en' : 'fr');
+            };
+
             $(document).on('click','.navbar-collapse.in',function(e) {
                 if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
                     $(this).collapse('hide');
