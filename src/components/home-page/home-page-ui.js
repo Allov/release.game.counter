12fede99cd469aa359a53949dfe359316a53198f
Allov <frederik.charest@gmail.com>
2015-02-28 18:26:45 -0500

    game name validation #13

--- src/components/home-page/home-page-ui.js
+++ src/components/home-page/home-page-ui.js
@@ -1,18 +1,32 @@
 define(['text!./home-page.html', 'knockout', 'knockout-i18next-translator'],
-    function(template, ko, Translator) {
+    function(template, ko, Translator, undefined) {
         'use strict';
         
         var ViewModel = function(params, componentInfo) {
             var self = this;
-            self.gameName = ko.observable();
-
+            
             self.translator = new Translator();
             self.t = self.translator.t;
+
+            self.content = ko.validatedObservable({
+                gameName: ko.observable(undefined).extend({
+                    required: {
+                        message: self.t('general.game-name-required')()
+                    }
+                })
+            }).extend({
+                bootstrapValidation: {}
+            });
         };
         
         ViewModel.prototype.create = function() {
             var self = this;
-            window.location = '/game/' + self.gameName();
+            self.content.isValidAsync().then(function(isValid) {
+                if (isValid) {
+                    window.location = '/game/' + self.content().gameName();
+                }
+            });
+            
         };
     
         ViewModel.prototype.dispose = function() {
